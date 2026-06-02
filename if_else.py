ad933b487476a2e672fb6a4366caf95ebf5259c8
saablx7-hub <saabl7@gmail.com>
2026-06-02 11:39:52 +0530

    2nd June - If and Else Exercise

---
+++ if_else.py
@@ -0,0 +1,11 @@
+#1. Write a program to check if a number is positive or negative
+a= -34
+if a < 0:
+    print("a is negative")
+
+#2. Write a Python program to check whether a number is even or odd using if-else
+a= float(input('ENTER NUMBER :'))
+if a%2 == 0:
+    print(f"[{a}] is Even")
+else:
+    print(f"{a} is odd")
