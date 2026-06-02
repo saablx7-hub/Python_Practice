7db8da98550a189cf2491038ca22b044c52c464c
saablx7-hub <saabl7@gmail.com>
2026-06-02 11:56:39 +0530

    2nd June If and Else

--- if_else.py
+++ if_else.py
@@ -1,4 +1,4 @@
-#1. Write a program to check if a number is positive or negative
+""" #1. Write a program to check if a number is positive or negative
 a= -34
 if a < 0:
     print("a is negative")
@@ -8,4 +8,21 @@ a= float(input('ENTER NUMBER :'))
 if a%2 == 0:
     print(f"[{a}] is Even")
 else:
-    print(f"{a} is odd")
+    print(f"{a} is odd") """
+
+#3. Write a program to input a person's age and print 'Minor' if less than 18, 'Adult' if between 18 and
+#60, and 'Senior' if 60 or above.
+a= float(input("Enter your age :"))
+if a < 18:
+    print("Minor")
+elif  a < 60:
+    print('Adult')
+elif a >= 60:
+    print('Senior')
+
+#4. Write a program that checks if a character entered is a vowel or a consonant
+a= (input('ENTER ALPHABET :'))
+if a == aeiouAEIOU:
+    print("vowel")
+else:
+    print("consonant")
